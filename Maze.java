83265564fa5e4c6db74e2fb11ed99ed3724b5385
lilkho <mkhondker@stuy.edu>
2019-02-27 20:19:37 -0500

    got array

--- Maze.java
+++ Maze.java
@@ -26,16 +26,27 @@ public class Maze {
   */
   public Maze(String file) throws FileNotFoundException {
     File mazeFile = new File(file);
-    Scanner mazeLines = new Scanner(mazeFile);
+    //find rows and columns
+    Scanner mazeLines1 = new Scanner(mazeFile);
+    int rows=0;
+    int cols=0;
+    while (mazeLines1.hasNextLine()) {
+      rows++;
+      cols=mazeLines1.nextLine().length();
+    }
+    maze=new char[rows][cols];
+    //create array
+    Scanner mazeLines2 = new Scanner(mazeFile);
     int i=0;
-    while (mazeLines.hasNextLine()) {
-      String line = mazeLines.nextLine();
+    while (mazeLines2.hasNextLine()) {
+      String line = mazeLines2.nextLine();
       for (int j=0;j<line.length();j++) {
         maze[i][j] = line.charAt(j);
       }
       i++;
     }
   }
+
   private void wait(int millis){
      try {
          Thread.sleep(millis);
@@ -45,103 +56,104 @@ public class Maze {
  }
 
 
-public void setAnimate(boolean b){
+  public void setAnimate(boolean b){
 
-    animate = b;
+      animate = b;
 
-}
-
-
-public void clearTerminal(){
-
-    //erase terminal, go to top left of screen.
-
-    System.out.println("\033[2J\033[1;1H");
-
-}
-
-
-
-
-
-
-/*Return the string that represents the maze.
-
- It should look like the text file with some characters replaced.
-
-*/
-public String toString(){
-  String ans="";
-  for (int i=0;i<maze.length;i++) {
-    for (int j=0;j<maze[i].length;j++) {
-      ans+=maze[i][j];
-    }
-    ans+="\n";
   }
-  return ans;
-}
+
+
+  public void clearTerminal(){
+
+      //erase terminal, go to top left of screen.
+
+      System.out.println("\033[2J\033[1;1H");
+
+  }
 
 
 
-/*Wrapper Solve Function returns the helper function
-
-  Note the helper function has the same name, but different parameters.
-  Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
-
-*/
-public int solve(){
-
-        //find the location of the S.
 
 
-        //erase the S
 
+  /*Return the string that represents the maze.
 
-        //and start solving at the location of the s.
+   It should look like the text file with some characters replaced.
 
-        //return solve(???,???);
-        return 0;
-}
-
-/*
-  Recursive Solve function:
-
-  A solved maze has a path marked with '@' from S to E.
-
-  Returns the number of @ symbols from S to E when the maze is solved,
-  Returns -1 when the maze has no solution.
-
-
-  Postcondition:
-
-    The S is replaced with '@' but the 'E' is not.
-
-    All visited spots that were not part of the solution are changed to '.'
-
-    All visited spots that are part of the solution are changed to '@'
-*/
-private int solve(int row, int col){ //you can add more parameters since this is private
-
-
-    //automatic animation! You are welcome.
-    if(animate){
-
-        clearTerminal();
-        System.out.println(this);
-
-        wait(20);
+  */
+  public String toString(){
+    String ans="";
+    for (int i=0;i<maze.length;i++) {
+      for (int j=0;j<maze[i].length;j++) {
+        ans+=maze[i][j];
+      }
+      ans+="\n";
     }
+    return ans;
+  }
 
-    //COMPLETE SOLVE
 
-    return -1; //so it compiles
-}
+
+  /*Wrapper Solve Function returns the helper function
+
+    Note the helper function has the same name, but different parameters.
+    Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
+
+  */
+  public int solve(){
+
+          //find the location of the S.
+
+
+          //erase the S
+
+
+          //and start solving at the location of the s.
+
+          //return solve(???,???);
+          return 0;
+  }
+
+  /*
+    Recursive Solve function:
+
+    A solved maze has a path marked with '@' from S to E.
+
+    Returns the number of @ symbols from S to E when the maze is solved,
+    Returns -1 when the maze has no solution.
+
+
+    Postcondition:
+
+      The S is replaced with '@' but the 'E' is not.
+
+      All visited spots that were not part of the solution are changed to '.'
+
+      All visited spots that are part of the solution are changed to '@'
+  */
+  private int solve(int row, int col){ //you can add more parameters since this is private
+
+
+      //automatic animation! You are welcome.
+      if(animate){
+
+          clearTerminal();
+          System.out.println(this);
+
+          wait(20);
+      }
+
+      //COMPLETE SOLVE
+
+      return -1; //so it compiles
+  }
 
 
   public static void main(String[] args) {
     try {
       Maze m = new Maze("Maze1.txt");
       System.out.println(m);
+      //System.out.println(maze[7][1]);
     } catch(FileNotFoundException f) {
       System.out.println("File doesn't exist");
     }
